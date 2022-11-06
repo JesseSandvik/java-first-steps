e9b01c8d01c5022eadea050e0bed52e2674ba06c
Jesse Sandvik <jsandvik89@gmail.com>
2022-11-06 13:52:21 -0500

    first java project

---
+++ src/com.codewithmosh/Main.java
@@ -0,0 +1,9 @@
+package com.codewithmosh;
+
+public class Main {
+    public static void main(String[] args) {
+        final int age = 33;
+        final int ourAge = age + 5;
+        System.out.println(ourAge);
+    }
+}
